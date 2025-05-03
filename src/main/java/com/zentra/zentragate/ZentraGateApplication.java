ed7977bb80cae6b1790bb526056198b631a1ba60
Pavan <pavansamaranayake@gmail.com>
2025-05-03 19:50:43 +0530

    Add initial files

---
+++ src/main/java/com/zentra/zentragate/ZentraGateApplication.java
@@ -0,0 +1,13 @@
+package com.zentra.zentragate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZentraGateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZentraGateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zentra/zentragate/ZentraGateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zentra.zentragate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZentraGateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
